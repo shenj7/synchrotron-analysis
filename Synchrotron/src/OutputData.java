df9930319f2bfad9f04e362b2ec4a6760bba9332
shenj7 <shenj7@192.168.11.180>
2021-06-12 13:05:09 -0400

    first commit

---
+++ Synchrotron/src/OutputData.java
@@ -0,0 +1,12 @@
+/*
+ * OutputData.java
+ * 
+ * @author: Jackson Shen
+ * 
+ * Generates data to graph within rstudio.
+ */
+public class OutputData {
+	public static void main(String[] args) {
+		
+	}
+}
